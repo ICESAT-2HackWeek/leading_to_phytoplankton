dfa0a05d257bad7285dcc24ee675de27eb5374b0
Romina Piunno <romina.piunno@mail.utoronto.ca>
2020-06-17 21:36:53 +0000

    included raise NotImplementedError

--- scripts/dataset_scripts/dataset.py
+++ scripts/dataset_scripts/dataset.py
@@ -1,3 +1,17 @@
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import h5py  
+from astropy.time import Time
+import sys  
+
+sys.path.insert(0, '/home/jovyan/leading_to_phytoplankton/scripts')
+import readers as rd
+
 class DataSet:
     
     '''
@@ -9,21 +23,21 @@ class DataSet:
     def __init__(self, boundingbox, timeframe):
         self.bounding_box = boundingbox
         self.time_frame = timeframe
-        pass
+        
     
     def visualize(self):
-        pass
+        raise NotImplementedError
     
     def download(self, out_path):
-        pass
+        raise NotImplementedError
     
     def get_meltpond_frcation(self):
-        pass
+        raise NotImplementedError
     
     def get_sea_ice_fraction(self):
-        pass
+        raise NotImplementedError
     
     def get_roughness(self):
-        pass
+        raise NotImplementedError
     
     
