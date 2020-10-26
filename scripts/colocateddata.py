4fbb4d0ab2c7b20dbb327ba965139f1b514ac8e0
Romina <romina.piunno@gmail.com>
2020-10-26 12:05:20 -0400

    began setting up the framework for show_area_overlap function to overlay plots of various datasets

--- scripts/colocateddata.py
+++ scripts/colocateddata.py
@@ -1,18 +1,34 @@
 from dataset_scripts import *
+import matplotlib.pyplot as plt
 
 
 class Colocateddata:
     
-    def __init__(self, boundingbox, time_start, time_end):
+    def __init__(self, boundingbox, time_start, time_end, proj='Default'):
         '''
         boundingbox: [lon1, lon2, lat1, lat2]
         time_start: datetime object
         time_end: datetime object
+        projection: a string name of projection to be used for plotting (e.g. 'Mercator', 'NorthPolarStereographic')
         '''
         self.bounding_box = boundingbox
         self.time_frame = [time_start, time_end]
         self.datasets = {}
-    
+        self.projection = self._determine_proj(proj)
+
+    def _determine_proj(self, proj):
+        if proj == 'Default':
+            # based on the bounding box specified, determine an appropriate projection
+            if min(self.bounding_box[2], self.bounding_box[3]) > 30:
+                return 'NorthPolarStereographic'
+            elif:
+                # todo: check other regions for most appropriate proj
+                #  ...
+                pass
+        else:
+            # todo: check that user entered valid proj name
+            return proj
+
     def __str__(self):
         '''
         Returns a string representation of self
@@ -29,7 +45,29 @@ class Colocateddata:
         
         return str
         
-    def show_area_overlap(self):
+    def show_area_overlap(self, show_datasets=None, porj='NorthPolarStereo'):
+        ds2plot = []
+        if show_datasets:
+            ds2plot = show_datasets
+        else:
+            ds2plot = self.datasets
+
+        # settings for plots that are common to all figures
+        # ...
+
+        plt.figure(figsize=(8, 8), dpi=600)
+        if self.projection == 'NorthPolarStereographic':
+            ax = plt.axes(projection=ccrs.NorthPolarStereo(central_longitude=0))  # choose polar sterographic for projection
+        elif self.projection == 'Mercator':
+            ax = plt.axes(projection=ccrs.Mercator(central_longitude=0))  # choose polar sterographic for projection
+
+        ax.coastlines(resolution='50m', color='black', linewidth=1)
+
+        for i in ds2plot:
+            # call individual plot functions from each dataset object
+            pass
+
+
         print('This will show all of the data overlapping on the same axes')
         pass
     
@@ -59,3 +97,7 @@ class Colocateddata:
     
     
 
+
+
+dat = Colocateddata([1,2,3,4], yesterday, today)
+dat.init_dataset([ATL03])
