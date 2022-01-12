aaedffe3d4e5117f9ae3042f40b99f2c253453eb
Romina <romina.piunno@gmail.com>
2022-01-12 16:33:34 -0500

    organisational notes and comments on future development goals

--- scripts/dataset_scripts/dataset.py
+++ scripts/dataset_scripts/dataset.py
@@ -22,6 +22,7 @@ class DataSet:
     
     def __init__(self, boundingbox, timeframe):
         '''
+        * use existing Icepyx functionality to initialise this
         :param timeframe: datetime
         '''
         self.bounding_box = boundingbox
@@ -38,17 +39,51 @@ class DataSet:
         raise NotImplementedError
 
     def _validate_input(self):
+        '''
+        This may already be done in icepyx.
+        Not sure if we need this here
+        '''
         raise NotImplementedError
 
     def search_data(self, delta_t):
+        '''
+        query dataset given the spatio temporal criteria
+        and other params specic to the dataset
+        '''
         raise NotImplementedError
 
-    def visualize(self):
-        raise NotImplementedError
-    
     def download(self, out_path):
+        '''
+        once data is querried, the user may choose to dowload the
+        data locally
+        '''
         raise NotImplementedError
-    
+
+
+    def visualize(self):
+        '''
+        (once data is downloaded)?, makes a quick plot showing where
+        data are located
+        e.g. Plots location of Argo profile or highlights ATL03 photon track
+        '''
+        raise NotImplementedError
+
+    def _add2colocated_plot(self):
+        '''
+        Takes visualise() functionality and adds the plot to central
+        plot with other coincident data. This will be called by
+        show_area_overlap() in Colocateddata class
+        '''
+        raise NotImplementedError
+
+
+
+    '''
+    The following are low priority functions
+    Not sure these are even worth keeping. Doesn't make sense for 
+    all datasets. 
+    '''
+
     def get_meltpond_fraction(self):
         raise NotImplementedError
     
@@ -58,5 +93,4 @@ class DataSet:
     def get_roughness(self):
         raise NotImplementedError
     
-    def _add2colocated_plot(self):
-        raise NotImplementedError
+
